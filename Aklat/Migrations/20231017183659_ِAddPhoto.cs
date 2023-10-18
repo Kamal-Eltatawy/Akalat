8020cd22aa02cc6de96098d7f2b8871334d97bb6
mohamed <mohamedhakk67@gmail.com>
2023-10-18 20:04:42 +0200

    PuLL

---
+++ Aklat/Migrations/20231017183659_ِAddPhoto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aklat.Migrations
+{
+    /// <inheritdoc />
+    public partial class _ِAddPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Products");
+        }
+    }
+}
