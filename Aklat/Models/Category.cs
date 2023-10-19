b5197f35ccda7f5684732d1297ce9aab05527901
kareemTaha006 <karimtaha006@gmail.com>
2023-10-19 18:42:24 +0300

    CategoryCrudCustomVaildition

--- Aklat/Models/Category.cs
+++ Aklat/Models/Category.cs
@@ -6,6 +6,8 @@ namespace Aklat.Models
     public class Category
     {
         public int ID { get; set; }
+
+        //[UniqueCategoryName(context:]
         [Required(ErrorMessage = "Category Name Cant Be Empity ")]
         [StringLength(maximumLength:50 , MinimumLength =3 ,ErrorMessage = "Category Name Must Be More Than 3 Less Than 50 Letters")]
         public string Name { get; set; }
